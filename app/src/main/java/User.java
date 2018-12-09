0af14986540d10691ecce9d24be8c394b3d7c0c8
MBOng124 <mitchell_ong@dlsu.edu.ph>
2018-12-10 00:29:55 +0800

    Updated FirebaseConnector class to FirebaseInterface class

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,9 +1,8 @@
 public class User {
     private String name, password;
 
-    public User(String name, String password) {
+    public User(String name) {
         this.name = name;
-        this.password = password;
     }
 
     public User() {
