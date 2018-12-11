0619cce4eafc4cb9756371e7fe64506ed25bc91d
MBOng124 <mitchell_ong@dlsu.edu.ph>
2018-12-12 00:25:54 +0800

    Modified the models for Users and CodeSample and added update functions in FirebaseInterface class

--- app/src/main/java/com/example/rachs/tutorapp_mobapde/CodeSample.java
+++ app/src/main/java/com/example/rachs/tutorapp_mobapde/CodeSample.java
@@ -1,17 +1,39 @@
 package com.example.rachs.tutorapp_mobapde;
 
 public class CodeSample {
-    private String codeSample, type, title, id;
+    private String codeSample, type, title, id, username, owner;
 
     public CodeSample() {
 
     }
 
-    public CodeSample(String codeSample, String type, String title, String id) {
+    public CodeSample(String codeSample, String type, String title, String id, String username, String owner) {
         this.codeSample = codeSample;
         this.type = type;
         this.title = title;
         this.id = id;
+        this.username = username;
+        this.owner = owner;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setCodeSample(String codeSample) {
